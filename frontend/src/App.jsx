bd5e6fc59798f2a74fd6987e4632a6e4499ab5d6
Gloria Lim <glorialimartt@gmail.com>
2024-01-25 14:43:09 -0500

    Refactor PhotoListItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,13 @@ const sampleDataForPhotoListItem = {
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
-const {id, imageSource, location, username, profile} =sampleDataForPhotoListItem
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem key={id} city={location.city} country={location.country} imageSource={imageSource} username={username} profile={profile}/>
+      <PhotoListItem photo={sampleDataForPhotoListItem}/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+  const {id, imageSource, location, username, profile} = props.photo;
   return (
-    <div className="PhotoListItem" key={props.key}>
-      <img src={props.imageSource}></img>
-      <img src={props.profile}></img>
-      <p>{props.username}</p>
-      <p>{props.city}, {props.country}</p>
+    <div className="PhotoListItem" key={id}>
+      <img src={imageSource}></img>
+      <img src={profile}></img>
+      <p>{username}</p>
+      <p>{location.city}, {location.country}</p>
     </div>
   )
 };
