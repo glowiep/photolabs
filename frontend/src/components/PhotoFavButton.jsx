f589d2581d081eb7504290b39db0b34d7f94dceb
Gloria Lim <glorialimartt@gmail.com>
2024-01-26 00:49:56 -0500

    Implement like button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,17 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+
 function PhotoFavButton() {
+  const [favorite, setFavorite] = useState(0)
+  
+  const handleClick = () => {
+    return setFavorite((prevFavorite) => (prevFavorite === 0 ? 1 : 0))  
+  }
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {<FavIcon />}
+      <div className="photo-list__fav-icon-svg" onClick={handleClick} >
+        {favorite === 0 ? <FavIcon /> : <FavIcon selected="true"/>}
       </div>
     </div>
   );
