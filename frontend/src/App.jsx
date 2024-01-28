9877a8d5cd2af7cb22ec8734707d7cc0ebac9a1d
Gloria Lim <glorialimartt@gmail.com>
2024-01-28 00:43:42 -0500

    Add PhotoDetailsModal route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics}/>
+      <PhotoDetailsModal />
     </div>
   );
 };
