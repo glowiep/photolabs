da35723bfe899356e0417a4f4df532e999a3e104
Gloria Lim <glorialimartt@gmail.com>
2024-01-28 16:28:35 -0500

    Update top nav bar style

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,8 +8,10 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics}/>
-      <FavBadge isFavPhotoExist={props.isFavPhotoExist} />
+      <div className="top-nav-bar">
+        <TopicList topics={props.topics}/>
+        <FavBadge isFavPhotoExist={props.isFavPhotoExist} />      
+      </div>
     </div>
   )
 }
