4a70e3122082187f6d38af59030bffcfb0259bdd
Gloria Lim <glorialimartt@gmail.com>
2024-02-03 23:39:56 -0500

    Add display ftooltip for favorites icon on top navigation bar

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import FavIcon from './FavIcon';
+import { Tooltip } from '@mui/material';
 
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
+    <Tooltip title="Click to display Favorites!">
     <div className='fav-badge'>
       <FavIcon displayAlert={!!isFavPhotoExist} selected={true}/>
     </div>
+    </Tooltip>
   ) 
 };
 
