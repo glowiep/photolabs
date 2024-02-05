7a2790e3db175062305e8d93ab5f0ec1712be877
Gloria Lim <glorialimartt@gmail.com>
2024-02-05 00:44:23 -0500

    Add loading skeleton

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
-import { Tooltip } from '@mui/material';
+import React, { useState } from 'react';
+import { Skeleton, Tooltip } from '@mui/material';
 
 import useApplicationData from 'hooks/useApplicationData';
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = ({ photo }) => {
-  const {urls, location, user} = photo;
-  const id = photo.id;
-  const { onPhotoSelect, state } = useApplicationData();
-  const { displayModal } = state;
+  
+  const [loading, setLoading] = useState(true)
+  setTimeout(()=>setLoading(false) , 1000)
 
-  return (
-    <div className="photo-list__item">
-        <PhotoFavButton id={id}/>
-        <Tooltip title="View Photo">
-          <img src={urls.regular} className={displayModal ? "photo-list__modal_image" : "photo-list__image"} onClick={() => {onPhotoSelect(photo)}} alt={`Regular size photo by ${user.username}`}></img>
-        </Tooltip>
-        <div className="photo-list__user-info photo-list__user-details">
-          <img src={user.profile} className="photo-list__user-profile" alt="User's profile picture"></img>
-          <div>
-            {user.username} 
-            <br>
-            </br>
-            <span className="photo-list__user-location">{location.city}, {location.country}</span>
+  if (loading) {
+    // Render skeleton for entire item if photo is not available
+    return (
+      <div className="photo-list__item">
+        <Skeleton variant="rectangular" width={300} height={250}/>
+      </div>
+    );
+  } else {
+    const {urls, location, user} = photo;
+    const id = photo.id;
+    const { onPhotoSelect, state } = useApplicationData();
+    const { displayModal } = state;
+  
+    return (
+      <div className="photo-list__item">
+          <PhotoFavButton id={id}/>
+          <Tooltip title="View Photo">
+            <img src={urls.regular} className={displayModal ? "photo-list__modal_image" : "photo-list__image"} onClick={() => {onPhotoSelect(photo)}} alt={`Regular size photo by ${user.username}`}></img>
+          </Tooltip>
+          <div className="photo-list__user-info photo-list__user-details">
+            <img src={user.profile} className="photo-list__user-profile" alt="User's profile picture"></img>
+            <div>
+              {user.username} 
+              <br />
+              <span className="photo-list__user-location">{location.city}, {location.country}</span>
+            </div>
           </div>
         </div>
-      </div>
-  )
+    )
+  }
+
+  
 };
 
 export default PhotoListItem;
