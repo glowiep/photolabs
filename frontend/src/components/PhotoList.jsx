929711297ebc51e77ac61019b88f9447da1640d2
Gloria Lim <glorialimartt@gmail.com>
2024-02-04 21:37:13 -0500

    Handle empty page

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
+import useApplicationData from "hooks/useApplicationData";
+import emptyImage from '../assets/empty.png';
 
 const PhotoList = ({ photos }) => {
+  const { state } = useApplicationData();
+
   return (
-    <ul className="photo-list">
+    <ul className='photo-list'>
+      {state.displayFavorites && photos.length === 0 && <img src={emptyImage} alt="No photos available"></img> }
       {photos.map((photo) => (
         <PhotoListItem 
           key={photo.id} 
