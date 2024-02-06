3b9666b7bb4eae4b6c20415508540b50903c4e5c
Gloria Lim <glorialimartt@gmail.com>
2024-02-06 00:45:20 -0500

    Add tooltip top to darkMode toggle

--- frontend/src/components/ControlledSwitch.jsx
+++ frontend/src/components/ControlledSwitch.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import useApplicationData from '../hooks/useApplicationData';
 
-import { Switch, FormGroup, FormControlLabel, CssBaseline } from '@mui/material';
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { Tooltip } from '@mui/material';
+import { Switch, FormGroup, FormControlLabel } from '@mui/material';
 
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
@@ -19,10 +19,12 @@ const ControlledSwitch = () => {
   return (
     <div>
       <FormGroup>
-        <FormControlLabel 
-          control={<Switch checked={darkMode} onChange={toggleDarkTheme}/>} 
-          label={darkMode ? <Brightness4Icon /> : <Brightness7Icon />}
-        />
+        <Tooltip title={`Toggle Dark Mode`}>
+          <FormControlLabel 
+            control={<Switch checked={darkMode} onChange={toggleDarkTheme}/>} 
+            label={darkMode ? <Brightness4Icon /> : <Brightness7Icon />}
+          />
+        </Tooltip>
       </FormGroup>
     </div>
   )
